e4422c44e8ddc3a6a23e9ef3d3efc80d4048e982
Christian Torrico <christian.torrico-contractor@procore.com>
2024-08-08 14:17:10 -0500

    updated Post model

--- app/src/main/java/com/locotoinnovations/composeviewpager/network/model/Post.kt
+++ app/src/main/java/com/locotoinnovations/composeviewpager/network/model/Post.kt
@@ -1,14 +1,8 @@
 package com.locotoinnovations.composeviewpager.network.model
 
-import com.google.gson.annotations.SerializedName
-
 data class Post(
-    @SerializedName("id")
-    val id: Int,
-    @SerializedName("userId")
-    val userId: Int,
-    @SerializedName("title")
     val body: String,
-    @SerializedName("body")
-    val title: String
+    val id: Int,
+    val title: String,
+    val userId: Int
 )
