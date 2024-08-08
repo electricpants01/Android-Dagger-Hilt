38a6d54a131bebcd4b8a0cf0fadc773f56c25169
Christian Torrico <christian.torrico-contractor@procore.com>
2024-08-08 14:16:51 -0500

    added converter factory GsonConverterFactory

--- app/src/main/java/com/locotoinnovations/composeviewpager/module/NetworkModule.kt
+++ app/src/main/java/com/locotoinnovations/composeviewpager/module/NetworkModule.kt
@@ -8,6 +8,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -25,6 +26,7 @@ object NetworkModule {
 
         return Retrofit.Builder()
             .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
             .client(httpClient.build())
             .build()
     }
