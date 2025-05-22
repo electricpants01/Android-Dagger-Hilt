497c951ee79bd465b449882f86a18b303612e40e
Christian Torrico <christian.torrico-contractor@procore.com>
2025-05-22 17:35:12 -0400

    removed unnecessary files

--- app/src/main/java/com/locotoinnovations/composeviewpager/network/PostService.kt
+++
@@ -1,9 +0,0 @@
-package com.locotoinnovations.composeviewpager.network
-
-import com.locotoinnovations.composeviewpager.network.model.Post
-import retrofit2.http.GET
-
-fun interface PostService {
-    @GET("posts")
-    suspend fun getPosts(): List<Post>
-}
--- app/src/main/java/com/locotoinnovations/composeviewpager/network/model/Post.kt
+++
@@ -1,8 +0,0 @@
-package com.locotoinnovations.composeviewpager.network.model
-
-data class Post(
-    val body: String,
-    val id: Int,
-    val title: String,
-    val userId: Int
-)
